7709aafab7136d472a105ff4ae2cf95723e0402b
Diego Vistoso <diegoivg98@gmail.com>
2023-03-08 18:10:56 -0300

    agregar componente Counter a App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import './App.css'
+import "./App.css";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
-     
+      <h1>useReducer Example</h1>
+      <Counter />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
