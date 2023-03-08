efa535a5baef3f4c07bec3ed6a6df1f6c7842d61
Diego Vistoso <diegoivg98@gmail.com>
2023-03-08 18:10:21 -0300

    creacion de contador con useReducer y context

---
+++ src/components/Counter.jsx
@@ -0,0 +1,55 @@
+import React, { useReducer } from "react";
+
+// ACTIONS
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+const RESET = "REST";
+
+/* Creating a context object. */
+const myContext = React.createContext(null);
+
+const Counter = () => {
+    //Estado inicial
+    const initialState = {
+        count: 0,
+    };
+
+    //Cambios de estados del Reducer
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case INCREMENT:
+                return {
+                    count: state.count + action.payload.quantity,
+                };
+
+            case DECREMENT:
+                return {
+                    count: state.count - action.payload.quantity,
+                };
+
+            case RESET:
+                return {
+                    count: 0,
+                };
+
+            default:
+                return state;
+        }
+    };
+
+    /* A hook that allows us to use the reducer function. */
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    return (
+        <myContext.Provider value={state}>
+            <div>
+                <p>Points: {state.count}</p>
+                <button onClick={() => dispatch({ type: INCREMENT, payload: { quantity: 1 } })}>INCREMENT</button>
+                <button onClick={() => dispatch({ type: DECREMENT, payload: { quantity: 1 } })}>DECREMENT</button>
+                <button onClick={() => dispatch({ type: RESET })}>RESET</button>
+            </div>
+        </myContext.Provider>
+    );
+};
+
+export default Counter;
